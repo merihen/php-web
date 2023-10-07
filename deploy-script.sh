3c2e2d9d9d5bac63f285ae229db5b4c9927e3734
merihen <60289251+merihen@users.noreply.github.com>
2023-10-07 21:35:51 +0100

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -7,9 +7,19 @@ DEPLOY_DIR="/home/master/applications/mgtwwtytpk/public_html"
 cd $DEPLOY_DIR
 
 # Pull the latest changes from your GitHub repository
-git pull origin-ssh test 
+git pull origin-ssh test
 
-# Optional: Update WordPress database (replace with your actual command)
-wp db update
+# Check if there are uncommitted changes
+if git diff-index --quiet HEAD --; then
+    # No uncommitted changes, proceed with deployment
+    echo "No local changes to commit."
+else
+    # Stash and pop the local changes
+    git stash
+    git stash pop
+fi
+
+# Update WordPress database (use 'wp db update' if applicable)
+wp core update-db
 
 echo "Deployment completed."
